09ef7c6f2762faddb906748220f3697cafcca6fc
Brian Donaldson <nairbsd@gmail.com>
2023-08-19 21:19:26 -0500

    init

---
+++ src/BLZR.Components.DragonDrop/DraggableItem.razor.js
@@ -0,0 +1,7 @@
+﻿function dragstart_handler(ev) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
+function addDragStartListener(elementId) {
+    document.getElementById(elementId).addEventListener("dragstart", dragstart_handler);
+}
